9be8d6c3d98e2025e61bd61bdf8c025234945929
CLEUMAN <cleuman11@hotmail.com>
2024-09-02 07:42:43 -0300

    refactor: changing the movie serializer to bring actor and genre data

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,5 +1,8 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
+from genres.serializers import GenreSerializer
+from actors.serializers import ActorSerializer
 from genres.models import Genre
 from actors.models import Actor
 
@@ -18,6 +21,26 @@ class MovieSerializer(serializers.ModelSerializer):
         if len(value) > 200:
             raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres.')
         return value
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True)
+    genre = GenreSerializer()
+    rate = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'actors', 'release_date', 'rate', 'resume']
+    
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+        if rate:
+            return round(rate, 1)
+        
+        return None
+    
+    
+        
         
 '''
 class MovieSerializer(serializers.Serializer):
